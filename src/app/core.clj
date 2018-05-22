e3ee39a7139576ad34c53284fa69c8c973fa0c2a
Michael Jeffrey <michaeldjeffrey@gmail.com>
2018-05-22 00:04:38 -0700

    Let's get a baseline by making a dumb program to play the game for us

--- src/app/core.clj
+++ src/app/core.clj
@@ -3,6 +3,7 @@
             [etaoin.keys :as keys]))
 
 (def my-driver (api/chrome))
+(api/quit my-driver)
 
 ;; I have a problem
 (api/go my-driver "https://gabrielecirulli.github.io/2048/")
@@ -40,6 +41,43 @@
     (let [[_ _ value _ _ x y] (clojure.string/split tile #"( |-)")]
       (map read-string [value x y]))))
 
-(tile-classes-to-input (tile-classes))
-  
+;; (tile-classes-to-input (tile-classes))
+
+(defn get-current-score []
+  (read-string
+   (api/get-element-text my-driver {:tag :div :fn/has-class "score-container"})))
+
+(defn game-over? []
+  (try
+    (api/query my-driver {:tag :div :fn/has-class "game-over"})
+    (catch Exception e false)))
+
+(defn restart-game []
+  (api/click my-driver {:tag :a :fn/has-class "restart-button"}))g
+
+;; (get-current-score)
+(def next-move (cycle [left down]))
+
+(def *default-right-threshold* 3)
+
+(defn play-dumb [restart?]
+  (when restart? (restart-game))
+  (let [previous-score (atom (get-current-score))
+        right-threshold (atom *default-right-threshold*)]
+    (doseq [move next-move
+            :while (not (game-over?))]
+      (move)
+      (let [current-score (get-current-score)]
+        (if (= @previous-score current-score)
+          (swap! right-threshold dec)
+          (do
+            (reset! right-threshold *default-right-threshold*)
+            (reset! previous-score current-score))))
+      (when (= @right-threshold 0)
+        (right)
+        (reset! right-threshold 3)))
+    (get-current-score)))
+
+(for [_ (range 5)]
+  (play-dumb (game-over?)))
 
