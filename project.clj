e84e62512638827b0749ba28a679c8422bece150
Michael Jeffrey <michaeldjeffrey@gmail.com>
2018-05-31 00:18:09 -0700

    add dep for dealing with file system nicer

--- project.clj
+++ project.clj
@@ -6,4 +6,5 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.trace "0.7.9"]
                  [etaoin "0.2.8-SNAPSHOT"]
-                 [lambda-ml "0.1.1"]])
+                 [lambda-ml "0.1.1"]
+                 [me.raynes/fs "1.4.6"]])
