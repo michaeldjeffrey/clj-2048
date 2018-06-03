5857627593a134722470f3b2d3fe93499192af9e
Michael Jeffrey <michaeldjeffrey@gmail.com>
2018-06-03 15:55:56 -0700

    You can play the game now.
    I keep two collections so I can treat everything as a reduce left.

--- src/app/game.clj
+++ src/app/game.clj
@@ -36,3 +36,45 @@
 (defn down [columns]
   (right columns))
 
+
+;; Top left (1, 1)
+;; Bottom right (4, 4)
+(def tiles {:x1y1 (atom 0) :x2y1 (atom 0) :x3y1 (atom 0) :x4y1 (atom 0)
+            :x1y2 (atom 0) :x2y2 (atom 0) :x3y2 (atom 0) :x4y2 (atom 0)
+            :x1y3 (atom 0) :x2y3 (atom 0) :x3y3 (atom 0) :x4y3 (atom 0)
+            :x1y4 (atom 0) :x2y4 (atom 0) :x3y4 (atom 0) :x4y4 (atom 0)})
+
+(def rows (partition 4
+                     (map tiles [:x1y1 :x2y1 :x3y1 :x4y1
+                                 :x1y2 :x2y2 :x3y2 :x4y2
+                                 :x1y3 :x2y3 :x3y3 :x4y3
+                                 :x1y4 :x2y4 :x3y4 :x4y4])))
+
+(def columns (partition 4
+                        (map tiles [:x1y1 :x1y2 :x1y3 :x1y4
+                                    :x2y1 :x2y2 :x2y3 :x2y4
+                                    :x3y1 :x3y2 :x3y3 :x3y4
+                                    :x4y1 :x4y2 :x4y3 :x4y4])))
+
+(def moves {:left  #(left rows)
+            :right #(right rows)
+            :up    #(up columns)
+            :down  #(down columns)})
+
+(defn add-random-tile []
+  (reset!
+   (rand-nth (filter #(zero? @%) (vals tiles)))
+   (if (> 0.9 (rand)) 2 4)))
+
+(defn add-random-tiles [n]
+  (repeatedly n add-random-tile))
+
+(defn print-game-board []
+  (doseq [row rows]
+    (println (map deref row))))
+
+(defn make-move [instruction]
+  (apply (instruction moves))
+  (add-random-tile)
+  (print-game-board))
+
